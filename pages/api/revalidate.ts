919a0fe5ef971e8a3abf1ab5cc3a4a93fc8565f0
teradonburi <daikiterai@gmail.com>
2022-07-03 12:12:28 +0900

    revalidate

---
+++ pages/api/revalidate.ts
@@ -0,0 +1,21 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+type Data = {
+  revalidated: boolean;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data | string>
+) {
+  const path = (req.query.path || '/') as string
+  try {
+    await res.revalidate(path);
+    return res.json({ revalidated: true });
+  } catch (err) {
+    // If there was an error, Next.js will continue
+    // to show the last successfully generated page
+    return res.status(500).send('Error revalidating');
+  }
+}
